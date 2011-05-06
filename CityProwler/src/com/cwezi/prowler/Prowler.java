632983c0fcccca646c4d8367644173cd857d7c52
Victor Miclovich <vicmiclovich@gmail.com>
2011-05-06 17:32:22 +0300

    added city prowler to gtug

---
+++ CityProwler/src/com/cwezi/prowler/Prowler.java
@@ -0,0 +1,13 @@
+package com.cwezi.prowler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Prowler extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
